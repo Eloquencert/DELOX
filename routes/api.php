69fa15c2d1d1b305fb2e9ceb14aa250d0577fcd8
vtk251_zhdo <vtk251_zhdo@student.ztu.edu.ua>
2026-05-16 16:32:22 +0300

    feat(messages): add message API routes and CSS bubble styles with entry animation

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,12 @@
 
 declare(strict_types=1);
 
+use App\Controllers\MessageController;
 use App\Controllers\UserController;
 
 /** @var \App\Core\Router $router */
 
 $router->get('/api/users/search', [UserController::class, 'search']);
+
+$router->get('/api/chats/:chatId/messages',  [MessageController::class, 'index']);
+$router->post('/api/chats/:chatId/messages', [MessageController::class, 'store']);
